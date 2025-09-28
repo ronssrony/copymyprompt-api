f18cc4241bfffa720a984f1d19b0efd6152ef0f0
rony <ronssrony@gmail.com>
2025-09-28 17:30:59 +0600

    Update TypeORM CLI config to include post, category, copy, like, rating, and share entities; add posts schema migration

--- src/config/typeorm-cli.config.ts
+++ src/config/typeorm-cli.config.ts
@@ -3,6 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 import { User } from '../user/entities/user.entity';
 import { InitialSchema1758471223836 } from '../migrations/intialSchema';
+import { PostsSchema1758475550000 } from '../migrations/postsSchema';
+import { Post } from '../posts/entities/post.entity';
+import { Category } from '../categories/entities/category.entity';
+import { Copies } from '../copies/entities/copy.entity';
+import { Like } from '../likes/entities/like.entity';
+import { Rating } from '../ratings/entities/rating.entity';
+import { Share } from '../shares/entities/share.entity';
 
 config();
 
@@ -17,6 +24,6 @@ export default new DataSource({
   database: configService.get<string>('DB_NAME'),
   logging: configService.get<boolean>('DB_LOGGING'),
   ssl: true,
-  entities: [User],
-  migrations: [InitialSchema1758471223836],
+  entities: [User, Post, Category, Copies, Like, Rating, Share],
+  migrations: [InitialSchema1758471223836, PostsSchema1758475550000],
 });
